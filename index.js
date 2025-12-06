4633b1e32f1f4f88f6ad6f00480822bb2b93588e
Abdullah <bsee22057@itu.edu.pk>
2025-12-06 16:54:52 +0500

    adding gitignore file

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-console.log("RUNNING FILE PATH:", __filename);
-
 const express = require('express');
 const app = express();
-const port = 4000;
 
 app.use(express.json());
 
-let lastRSSI = {};   // store latest data
+let lastData = {};
 
 app.post("/rssi", (req, res) => {
-    console.log("📡 Data received:", req.body);
-    lastRSSI = req.body;
-    res.json({ status: "ok" });
+    console.log("📡 Data from ESP32:", req.body);
+    lastData = req.body;
+    res.json({ status: "received" });
 });
 
-// Show data in browser
 app.get("/", (req, res) => {
     res.json({
-        message: "Last ESP32 Data:",
-        data: lastRSSI
+        msg: "ESP32 Server Online on Render",
+        lastData: lastData
     });
 });
 
-app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
+// Render assigns the port automatically
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
 });
