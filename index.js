3322562a0c582c5161b9830f0f5f5bfd2ac69122
Abdullah <bsee22057@itu.edu.pk>
2025-12-06 17:14:02 +0500

    chande in port

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 });
 
 // Render assigns the port automatically
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
 });
