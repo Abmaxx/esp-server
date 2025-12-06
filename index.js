5b109a3218ef5ee3c49beccbe57884d892a520f5
Abdullah <bsee22057@itu.edu.pk>
2025-12-06 17:27:16 +0500

    changed port

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 });
 
 // Render assigns the port automatically
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8800;
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
 });
