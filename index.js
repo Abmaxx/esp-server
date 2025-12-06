9e0f392c35a0b1cc068d3e2cbdf53caa6ec7466d
Abdullah <bsee22057@itu.edu.pk>
2025-12-06 18:16:10 +0500

    the updated code

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
+// const express = require('express');
+// const app = express();
+
+// app.use(express.json());
+
+// let lastData = {};
+
+// app.post("/rssi", (req, res) => {
+//     console.log("📡 Data from ESP32:", req.body);
+//     lastData = req.body;
+//     res.json({ status: "received" });
+// });
+
+// app.get("/", (req, res) => {
+//     res.json({
+//         msg: "ESP32 Server Online on Render",
+//         lastData: lastData
+//     });
+// });
+
+// // Render assigns the port automatically
+// const PORT = process.env.PORT || 8800;
+// app.listen(PORT, () => {
+//     console.log(`Server running on PORT ${PORT}`);
+// });
+
+
+
 const express = require('express');
 const app = express();
-
-app.use(express.json());
-
-let lastData = {};
-
-app.post("/rssi", (req, res) => {
-    console.log("📡 Data from ESP32:", req.body);
-    lastData = req.body;
-    res.json({ status: "received" });
-});
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-    res.json({
-        msg: "ESP32 Server Online on Render",
-        lastData: lastData
-    });
+    res.send("Hello World! Server is running.");
 });
 
-// Render assigns the port automatically
-const PORT = process.env.PORT || 8800;
-app.listen(PORT, () => {
-    console.log(`Server running on PORT ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
 });
